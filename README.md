8ecc42b5f5c8a8ec7c4e341058646717e611aa76
bthan-daa <113547400+bthan00@users.noreply.github.com>
2025-01-06 23:36:54 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Automated Finance Jupyter Notebook
+
+A Docker based solution leveraging Plaid API to automate and store financial data in PostgreSQL for analysis in Jupyter Notebooks. 
+
+This product is in pre-release stage.
+
+## Installation
+
+Set up your variables in .env.example
+
+```bash
+cp .env.example
+```
+
+## Usage
+
+```bash
+docker-compose up
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ db/postgres/init.sql
@@ -0,0 +1,62 @@
+-- Encryption extensions to secure access_keys to Plaid
+CREATE EXTENSION pgcrypto ;
+
+-- Create SQL structure to accept Plaid API data
+
+CREATE TABLE accounts (
+    account_id VARCHAR(255) PRIMARY KEY,
+    mask VARCHAR(255),
+    name VARCHAR(255),
+    official_name VARCHAR(255),
+    persistent_account_id VARCHAR(255),
+    subtype VARCHAR(255),
+    type VARCHAR(255),
+    user_email VARCHAR(255),
+    user_phone VARCHAR(20),
+    plaid_access_token VARCHAR(255)
+);
+
+CREATE TABLE accounts_balance_history (
+    id SERIAL PRIMARY KEY,  
+    account_id VARCHAR(255),
+    balances_available FLOAT,
+    balances_current FLOAT,
+    balances_iso_currency_code VARCHAR(10),
+    balances_limit FLOAT,
+    balances_unofficial_currency_code VARCHAR(10),
+    balances_datetime TIMESTAMP,
+    CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+);
+
+
+CREATE TABLE transactions (
+    transaction_id VARCHAR(255) PRIMARY KEY,
+    account_id VARCHAR(255), 
+    amount FLOAT,
+    authorized_date	date,
+    category_id	VARCHAR(20), 
+	date date,
+    iso_currency_code VARCHAR(20),
+    logo_url VARCHAR(255), 
+    merchant_entity_id VARCHAR(255), 
+    merchant_name VARCHAR(255), 
+    name VARCHAR(255), 
+    payment_channel	VARCHAR(255), 
+    pending BOOLEAN,
+    personal_finance_category_icon_url VARCHAR(255), 
+    website VARCHAR(255),
+    personal_finance_category_detailed VARCHAR(255),
+    personal_finance_category VARCHAR(255),
+    CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+
+);
+
+-- Analytical views
+
+CREATE VIEW v_latest_account_balance AS
+SELECT DISTINCT ON (account_id) *
+FROM accounts_balance_history
+ORDER BY account_id, balances_datetime DESC;
+
+
+
